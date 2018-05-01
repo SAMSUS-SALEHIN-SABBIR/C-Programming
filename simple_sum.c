ad70b902d6903c5262e1dc38de5fcd7412cd0ea0
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:39:14 +0600

    Create simple_sum.c

---
+++ simple_sum.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// sum korar programme
+
+
+int main()
+
+
+//Sob kisu ami nijei diye dilam
+// note : new line (\n) dewa onk valo
+// semecolon er prob tar uporer line e hoy bt nicer line e dekhay
+
+/*
+{
+    int a;
+    int b;
+    int sum;
+    a = 10;
+    printf("A is %d\n",a); // %d te ami a er man nilam . ("") er por (,) diye alada kore nite hoy
+    b = 15;
+    printf("B is %d\n",b);
+    sum = a+b;
+    printf("A + B = %d\n",sum);
+    printf("%d + %d = %d\n",a,b,sum);
+    return 0;
+}
+
+*/
+// man ekbare assain korbo ekline ei likhbo
+
+/*
+{
+
+    int a,b,sum;
+    a = 10;
+    b = 45;
+    sum = a + b;
+
+
+    // abar ekbare kora jay kajta
+
+    int a=10,b=45,sum=a+b; // koma (,) diye diye alada korlam
+
+    printf("sum = %d\n",sum);
+    return 0;
+}
+
+
+// besir  vag somoy new line dewa vule jai amra bt eta vula jabe na
+
+*/
+
+// /* issa moto dilam */ bt er moddhe abar /* ja issa */ dewa jabe na
+// bastob sonkhar sum ber korbo
+
+{
+    double a=12.3,b=23.14,sum=a+b; // sum int e dile kintu amader . er por er number bad porbe
+    printf("sum is %lf\n",sum);
+    printf("sum is %.2lf\n",sum); // .2 dilam karon ami . er por 2 sonkha dekhte cai tar besi na
+    return 0;
+
+}
+
