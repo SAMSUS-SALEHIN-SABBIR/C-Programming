bbf7206eccc95b55cbded7fe4aec8a0a9de3f79e
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:47:40 +0600

    Create smal or big number cheak.c

---
+++ smal or big number cheak.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n<30)
+        printf("This number is less than 30\n");
+    else if(n<50)
+        printf("THis Number is less than 50\n");
+    return 0;
+}
+//n=10 hole output this number is less than 30
+// jodio 2ns condition taw sotti bt seta count hobe na
+// bcz first if sotto hole es=lse if or else sottto kina seta dekhe na
+//2 ta if thakle 2 tai sothik
+
