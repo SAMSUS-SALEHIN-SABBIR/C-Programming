55145eb9489dd2973f442ab7851f865efa641b87
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:46:52 +0600

    Create positive negetive cheak.c

---
+++ positive negetive cheak.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+
+/*
+{
+    int n;
+    scanf("%d",&n);
+    if(n<0){
+        printf("This Number Is Negative\n");
+    }
+    else{
+        printf("This Number Is Positive\n");
+    }
+    return 0;
+}
+*/
+//assa amra eibar ektu sort korbo ei programme ta
+// dekhte cai je {} sara hoy naki
+
+/*
+{
+    int n;
+    scanf("%d",&n);
+    if(n<0)
+        printf("This Number Is Negative\n");
+    else
+        printf("This Number Is Positive\n");
+    return 0;
+}
+*/
+
+/*
+{
+    int n;
+    scanf("%d",&n);
+    if(n<0)
+        printf("This number is Negetive\n");
+    else if (n==0)
+        printf("This Number is zero\n"); //onekgula condition jacai korar khettre eta use kora hoy
+    else
+    printf("This number Is Positive\n");
+    return 0;
+}
+*/
+
+// if else 2 tai use kora lagbe emn kono kotha nai
+// ekta sonkha 10 er theke boro kina cheak koro
+
+
+{
+    int n;
+    scanf("%d",&n);
+    if(n>10)
+    printf("This number is greater than 10\n");
+    return 0;
+}
+//evabe programme lekha tmn ekta full tatporjo pawa jay na
+//10 er kom output dile to kisui dekhabe na tai na?
