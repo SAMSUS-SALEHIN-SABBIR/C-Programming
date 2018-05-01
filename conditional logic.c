d6e1c320261c6f51ad2bc8ac10b75a6f5f57639b
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:45:16 +0600

    Create conditional logic.c

---
+++ conditional logic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//positive or negetive cheak
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>=0)//if mane jodi . if er ( er moddhe ) condition sotti hole  { er kaj korbe}
+        {
+        printf("The Number Is Positive\n");
+    }
+    else// ar ta na hole else er kaj korbe
+        {
+        printf("The number is Negetive\n");
+    }
+    return 0;
+}
+
+// < mane soto . > mane boro
+// <= sto ba soman
+// >= boro ba soman
+// == mane soman
+// != soman na
