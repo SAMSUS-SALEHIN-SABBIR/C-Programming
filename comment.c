e9ae5b8d28ccfff16d0a255105ca6e4c5e6889b2
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:45:55 +0600

    Create comment.c

---
+++ comment.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //test programm -comment 1
+    // ek line er comment er jnnno use kora hoy
+    /*ami ek line likhlam
+    2 line likhlam
+    joto issa toto likhlam
+    */
+    //uporer coment gula malti comment ar eita ek line comment
+    //ei line e ja issa likhbo bt compile seta dhorbe na
+
+    printf("Hello world!\n");
+    return 0;
+}
+//token ki ?
+ // variable er nam ta ek ba ekadhik okkhrer hobe jmn: valu
+ //kintu age digit hobe nah jmn: 0valu
+ //space use kora jabe nah bt _ use kora jabe jmn: i_love_you
+ // programm er sathe manan soi variable name dewya valo
