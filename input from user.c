79b426b71e93cd69b5be647b636ffc63b7be5a1b
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:55:01 +0600

    Create input from user.c

---
+++ input from user.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+
+/*
+{
+    int a,b,sum;
+    scanf("%d %d",&a,&b);//scanf dhara input neya hoy. jar input nebo tar age & lagate hoy
+    printf("%d\n",a+b);//ekhane initialize korar theke age kora valo
+    return 0;
+}
+*/
+
+
+
+// 2bar er besi 3 bar input nile seti nibe na
+// & na diloe segmentation error dekhay
+
+//charecter type er data type
+
+/*
+{
+    char charecter;
+    printf("Enter  a charecter \n");
+    //scanf("%c",&charecter); // %s dile null dekhay %c dile ekta charecter dekhay.
+    //getchar(charecter);// evabe dile hobe na
+    charecter=getchar();//eta perfect
+    printf("%c\n\n",charecter);
+    return 0;
+}
+*/
+
+//upore to input nilam bt ebar direct assain korbo
+{
+    //char c=amar;//ekane ekta error ase seta holo 1 ta charecter nebe bt amra besi diyesi
+    //char c=a;//ekane problem holo amra char variable e direct man nite parbo na 'er moddhe' nite hobe
+    char c='a';
+    printf("%c\n",c);
+    return 0;
+}
