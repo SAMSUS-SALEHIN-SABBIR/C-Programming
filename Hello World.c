94c7ef8612a41a1b46aa0a319b961b735e61cf52
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:36:11 +0600

    Create Hello World.c

---
+++ Hello World.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+
+// void main use korle time kom lage.
+// return 0 na dileo kaj hoy.
+// prinf function e ekadhik bar double qutation ney na
+// semecolon na dile error dekhay
+
+/*
+void main()
+{
+    printf("Hello World");
+}
+*/
+
+int main()
+
+
+{
+    printf("Hello World");
+    return 0;
+}
+/*
+exemple print I Love My Country, Bangladesh
+ans :
+int main()
+{
+printf("I Love My Country, Bangladesh");
+return 0;
+}
+*/
