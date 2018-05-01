4c850bc9bdd6758878e59c8db6441962675348e3
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:51:19 +0600

    Create or operator.c

---
+++ or operator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num>=1 || num<=10)
+        printf("Conditional Number\n");
+    else
+        printf("Wrong Number\n");
+    return 0;
+}
+//ei programm tay ami num = 100 dileo yes
+//abar -54 dileo yes dekhabe
+// karon holo 2 ta kondition er ekta sotto manei condition ta sotto
+// || operetor 1 0 or 1 1 or 1 1 or 0 0 jai hok na keno tar mani sotto
