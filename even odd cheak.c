2f472440395f6cf89b84772eb8ba30a3a2aada24
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:48:57 +0600

    Create even odd cheak.c

---
+++ even odd cheak.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+/*
+{
+    int num,reminder;
+    scanf("%d",&n);
+    reminder=n%2;
+    if(reminder==0)
+        printf("This number is even\n");
+    else
+        printf("This Number Is odd\n");
+    return 0;
+}
+*/
+//reminder sarao amra kajta korte partam
+
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==0)
+        printf("This Number Is Even\n");
+    else
+    printf("This Number Is Odd\n");
+    return 0;
+}
