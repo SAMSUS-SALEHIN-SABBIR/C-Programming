18b45189a34abdbf98257cb1ce85def05fb4793f
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 01:53:45 +0600

    Create while loop and its some wrong.c

---
+++ while loop and its some wrong.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+/*
+{
+    int n=1;
+    while(n<10){
+        printf("%d\n",n);
+    }
+    n++;
+    return 0;
+}
+*/
+// mojar bepar codte ta run korle 1 print kortese proti line e
+// ar sesh hosse na loop ta coltese to coltesei
+// er karon ki?
+// n= 1 korlam
+//while e gelo ,condition cheak korlo , vitorer kaj korlo
+// n er man kintu 1 e theke gelo
+// tai 1 abar while e gelo abar cheak kore condition sotto holo
+// than abar print korlo , abaro n er man 1 e roye gelo
+// evabe coltei ase sesh na bcz 1 to sotto potibar
+// asole vul ki koresilam tai le ?
+// n++ ta amra loop er baire rekhesilam
+//vitore rakhle man barto nd eksomoy 10 er besi hole programm ta theke jeto
+// nd loop theke ber hoye porer kaj gula kore output o thik asto
+{
+    int n=1;
+    while(n<10){
+        printf("%d\n",n);
+        n++;
+    }
+    return 0;
+}
+//ekhane amader ekta kota janar ase
+//amra keno while er () er pore ; use korlam nah ?
+// amra semiclone dile okhanei programm ta sesh hoye jabe
+// ar poro borti loop er block e dhukbe na
+// amader jana dorkar {} thakle tar pore ;  dite hoy na
