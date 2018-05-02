51b70f06a26c4f4e8fbfe95f275abc93f444f46a
Samsus Salehin Sabbir <34869213+SAMSUS-SALEHIN-SABBIR@users.noreply.github.com>
2018-05-02 13:01:16 +0600

    Create basic string.c

---
+++ basic string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+// simple
+/*int main()
+{
+    char name[]= {'s','a','b','b','i','r'};
+
+    printf("%s\n",name);
+    return 0;
+}
+*/
+//
+int main()
+{
+    char name[]= {'s','a','b','\0','b','i','r'};
+
+    printf("%s\n",name);
+    return 0;
+}
